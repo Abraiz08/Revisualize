f73e0e382a9240318b0779e150a4ed8a7bee3207
Abraiz Khan <92964846+Abraiz08@users.noreply.github.com>
2026-05-11 01:12:59 -0700

    Got image generation through API working

--- src/main.py
+++ src/main.py
@@ -1,9 +1,26 @@
 from google import genai
+from PIL import Image
 
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
 client = genai.Client()
 
-response = client.models.generate_content(
-    model="gemini-3-flash-preview", contents="Explain how AI works in a few words"
-)
-print(response.text)
+def main():
+    
+    scene = input("Scene: ")
+    response = generateImage(scene)
+    for part in response.parts:
+        if part.text is not None:
+            print(part.text)
+        elif part.inline_data is not None:
+            image = part.as_image()
+            image.save("generated_image.png")
+
+def generateImage(scene):
+    response = client.models.generate_content(
+        model = "gemini-3.1-flash-image-preview",
+        contents = "Generate an image of " + scene
+    )
+    return response
+
+if __name__ == "__main__":
+    main()
