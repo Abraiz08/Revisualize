9454ddcaa20bde1b9d8fd7a56bfda6e3cce07b7b
Abraiz Khan <92964846+Abraiz08@users.noreply.github.com>
2026-05-12 17:10:40 -0700

    Added ability to choose number of panels
    Generates a JSON Array with chosen number of panels

--- src/main.py
+++ src/main.py
@@ -36,20 +36,22 @@ from PIL import Image
 #   }
 # }
 
+#TODO maybe try spliutting the file into line by line and then sending to ai with the line at which the last visual beat left off, that way we wont have to send the whole wall of text everytime, possibly using less tokens?
 
 client = genai.Client()
 
-PROMPT = 'Act as a storyboard artist. Given a script or a scene, find the first visual beat for which to create a storyboard panel, analyze it and output a JSOn object strictly in the form:' \
-'\n{\n"panel_number":,\n"visual_description": {\n\t"subject":\n\t"action":\n\t"setting":\n\t"key_elements":[]\n\t}\n}'\
+PROMPT = 'Act as a storyboard artist. Given a script or a scene, find the next visual beat for which to create a storyboard panel, analyze it and output a JSOn object strictly in the form:' \
+'\n{\n"visual_description": {\n\t"subject":\n\t"action":\n\t"setting":\n\t"key_elements":[]\n\t}\n}'\
 '\nOutput the JSON object and nothing else. This is the script/scene:\n'
 
+CONTEXT = '\nThis is all the context so far, the next visual beat should be after all this context:\n'
 
 def main():
-    with open('data/scene.txt', 'r') as file:
-        data = file.read()
-    scene = data
-    decomposeSceneToJSON(scene)
-    # response = generateImage(scene)
+    #TODO make this typesafe
+
+    num_panels = input("Enter number of panels: ")
+    createSceneObjects(int(num_panels))
+      # response = generateImage(scene)
     # for part in response.parts:
     #     if part.text is not None:
     #         print(part.text)
@@ -57,14 +59,25 @@ def main():
     #         image = part.as_image()
     #         image.save("output/generated_image.png")
 
-def decomposeSceneToJSON(scene):
+def createSceneObjects(num_panels: int):
+    with open('data/scene.txt', 'r') as file:
+        data = file.read()
+    scene = data
+    context = ''
+    #TODO prime the AI to split the scene into num_panels visual beats first before running this
+    for i in range(num_panels):
+        context += "panel number: " + str(i) + "\n" + decomposeSceneToJSON(scene, context) + "\n"
+    print(context)
+
+def decomposeSceneToJSON(scene: str, context: str) -> str:
+    #TODO runs too many times with increasing prompt size
     response = response = client.models.generate_content(
         model = "gemini-3-flash-preview",
-        contents = PROMPT + scene
+        contents = PROMPT + scene + CONTEXT + context
     )
-    print(response.text)
+    return response.text
 
-def generateImage(scene):
+def generateImage(scene: str) -> genai.types.GenerateContentResponse:
     response = client.models.generate_content(
         model = "gemini-3.1-flash-image-preview",
         contents = "Generate an image of " + scene
